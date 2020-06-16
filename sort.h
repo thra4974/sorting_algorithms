0d406f5f3cb0e0fa78b8d4e0196225fbf2043d70
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-16 11:16:27 -0500

    added 3- quick sort

--- sort.h
+++ sort.h
@@ -27,11 +27,14 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 /** Helper function prototypes **/
 void swap(int *n1, int *n2);
 void sort_insert(listint_t **list, listint_t *new);
 void sort_insert(listint_t **list, listint_t *new);
 listint_t *create_node(int n);
+ssize_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size);
+void quicksort(int *array, ssize_t low, ssize_t high, size_t size);
 
 #endif
