52d72f16bd0e98489fd8564c9beec680f23ad918
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-17 18:18:22 -0500

    fixed for loop

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,8 +28,9 @@ ssize_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
 {
 	int pivot = array[high];
 	ssize_t p = (low - 1);
+	ssize_t j;
 
-	for (ssize_t j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 /** Helper function prototypes **/
 void swap(int *n1, int *n2);
